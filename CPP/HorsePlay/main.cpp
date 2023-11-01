a43fb13213acca3ea7d44e34f8e70124298b818b
soulus2 <73872349+soulus2@users.noreply.github.com>
2023-11-01 09:52:03 +0400

    not gut

---
+++ CPP/HorsePlay/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+#include <iomanip>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int x =0;
+	srand(time(0));
+	while(true){
+	dice100
+	}
+	
+	return 0;
+}
